df1c9b83bb22dc8c97f3a9df3aef4e81e830b257
Scotty Moon <scotty.moon@gmail.com>
2020-12-26 20:34:59 -0600

    images, svg, header

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,15 @@
 import { Route, Switch } from "react-router-dom"
 import HomePage from "../../pages/HomePage"
 import ShopPage from "../../pages/ShopPage"
+import Header from "../Header"
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
         <Route path="/shop/:item" component={ShopPage} />
       </Switch>
     </div>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import "./styles.scss"
+import { ReactComponent as Logo } from "../../assets/svg/crown.svg"
+
+export default function Header() {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          Shop
+        </Link>
+        <Link className="option" to="/shop">
+          Contact
+        </Link>
+      </div>
+    </div>
+  )
+}
